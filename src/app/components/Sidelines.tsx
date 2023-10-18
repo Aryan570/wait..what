fda8a6d12af8f95f760ba082b79d8b7a755ec726
Aryan <malhotraaryan570@gmail.com>
2023-10-18 21:57:34 +0530

    yo5_2

--- src/app/components/Sidelines.tsx
+++ src/app/components/Sidelines.tsx
@@ -14,7 +14,12 @@ const Sidelines = async () => {
         password: session.password
     })
     const urlR = res ? res.fileURL : '/fatrat.jpg'; ///have to do something here
-    const posts = res?.posts;
+    const resPost = res?.posts;
+    // console.log(resPost)
+    interface Ele{
+        title: string,
+        content: string
+    }
     return (
         <div>
             <div className=' grid grid-cols-6 grid-rows-6'>
@@ -23,12 +28,21 @@ const Sidelines = async () => {
                     <div className="bg-[url('/chekkkss.svg')] z-10 w-full h-full absolute"></div>
                     <Image className='object-cover ' src={urlR} alt='Cover Picture' fill={true} />
                 </div>
-                <div className='col-start-2 min-h-screen  col-span-4 row-start-2 row-span-4 text-center border-l-[0.5px] border-r-[0.5px] border-dashed'>3</div>
+                {/* <div className='col-start-2 min-h-screen  col-span-4 row-start-2 row-span-4 text-center border-l-[0.5px] border-r-[0.5px] border-dashed'>3</div> */}
+                <div className='col-start-2 min-h-screen  col-span-4 row-start-2 row-span-4 border-l-[0.5px] border-r-[0.5px] border-dashed'>
+                    {resPost.map((ele : Ele) =>(
+                        <div className='bg-slate-800 rounded-xl m-1 py-2' key={ele.title}>
+                            <div className='p-2 text-lg font-bold'>{ele.title}</div>
+                            <hr className=' mx-2 opacity-70'/>
+                            <div className='p-2'>{ele.content}</div>
+                        </div>
+                    ))}
+                </div>
                 <div className='col-start-6 col-span-1 row-start-1 row-span-1 text-center border-b-[0.5px] border-dashed'>
-                    <Avatar>
+                    {/* <Avatar>
                         <AvatarImage src={urlR} />
-                        <AvatarFallback>WW</AvatarFallback>
-                    </Avatar>
+                        <AvatarFallback>WW</AvatarFallback>  We'll see if we want to use avatar
+                    </Avatar> */}
                     <SheetDemo />
                 </div>
             </div>
