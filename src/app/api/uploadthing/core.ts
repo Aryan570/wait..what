e8e1e692d265405ac6b3982633426cfe7dc8e966
Aryan <malhotraaryan570@gmail.com>
2024-07-27 19:26:29 +0530

    fixed_uploadThing_file_url

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -30,7 +30,7 @@ export const ourFileRouter = {
       },
       {
         $set:{
-            fileURL : `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
+            fileURL : `${file.url}`
         },
       })
       console.log("Upload complete for userId:", metadata.userId);
